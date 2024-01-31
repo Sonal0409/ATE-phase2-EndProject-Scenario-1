b4420988c07e32acc93072dbd93422e5e258892f
sonal0409 <mittal.sonal04@gmail.com>
2024-01-31 13:06:31 +0530

    done

---
+++ src/main/java/ATE_Phase2EndProject_Cucumber_Junit_Scenario1/ATE_Phase2EndProject_Cucumber_Junit_Scenario1/App.java
@@ -0,0 +1,13 @@
+package ATE_Phase2EndProject_Cucumber_Junit_Scenario1.ATE_Phase2EndProject_Cucumber_Junit_Scenario1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
